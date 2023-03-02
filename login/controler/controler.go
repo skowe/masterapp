4fbc89864fa2736555bfe356d39748f7d951a1a3
skowe <veselin.petrovic.97@gmail.com>
2023-03-02 16:26:34 +0100

    Login nastavljen

--- login/controler/controler.go
+++ login/controler/controler.go
@@ -1,11 +1,14 @@
 package controler
 
 import (
+	"bytes"
 	"database/sql"
+	"encoding/json"
 	"fmt"
 	"net/http"
 
 	"github.com/skowe/masterapp/libs/config"
+	"github.com/skowe/masterapp/models"
 )
 
 const (
@@ -31,19 +34,63 @@ func (reg *LoginHandle) Open() (*sql.DB, error) {
 	return sql.Open("mysql", dbConnString)
 }
 
-func (l *LoginHandle) ServeHTTP(w http.ResponseWriter, r *http.Request){
-	
-	http.Post(l.SessionHost, "application/json", )
+func (l *LoginHandle) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	responseEncoder := json.NewEncoder(w)
+
+	user, err := extractUserFromRequest(r)
+	if err != nil {
+		responseEncoder.Encode(&models.ApiError{
+			ErrorCode:    http.StatusBadRequest,
+			ErrorMessage: "Error 400: bad request",
+			ErrorType:    "bad request",
+			Info:         "Malformed request body, can't extract user data",
+		})
+		return
+	}
+
+	sessionResponse, err := GetSessionParams(user, l)
+	if err != nil {
+		responseEncoder.Encode(&models.ApiError{
+			ErrorCode:    http.StatusInternalServerError,
+			ErrorMessage: "Error 500: internaln server error",
+			ErrorType:    "internaln server error",
+			Info:         "Failed to create a session",
+		})
+		return
+	}
+	sessionDecoder := json.NewDecoder(sessionResponse.Body)
 }
 
 func New() *LoginHandle {
 	env := config.Configure(append([]string{SESSION_HOST_ADDRESS}, config.GetDbEnvNames()...))
 	return &LoginHandle{
-		DbHost: env[config.DB_HOST],
-		DbPort: env[config.DB_PORT],
-		DbName: env[config.DB_NAME],
-		DbUser: env[config.DB_USER],
-		DbPass: env[config.DB_PASS],
+		DbHost:      env[config.DB_HOST],
+		DbPort:      env[config.DB_PORT],
+		DbName:      env[config.DB_NAME],
+		DbUser:      env[config.DB_USER],
+		DbPass:      env[config.DB_PASS],
 		SessionHost: env[SESSION_HOST_ADDRESS],
 	}
+}
+
+func extractUserFromRequest(r *http.Request) (*models.User, error) {
+
+	decoder := json.NewDecoder(r.Body)
+
+	user := &models.User{}
+	err := decoder.Decode(user)
+	if err != nil {
+		return nil, err
+	}
+	defer r.Body.Close()
+
+	return user, nil
+}
+
+func GetSessionParams(user *models.User, l *LoginHandle) (*http.Response, error) {
+	userEncoded, err := json.MarshalIndent(user, "", "\t")
+	// Ovde je neophodno napraviti pozeljni reader objekat za post metod ka session menadzeru
+	RequestBody := bytes.NewReader(userEncoded)
+	return http.Post(l.SessionHost+"/start", "application/json", RequestBody)
 }
--- login/go.mod
+++ login/go.mod
@@ -2,4 +2,7 @@ module github.com/skowe/masterapp/login
 
 go 1.20
 
-require github.com/skowe/masterapp/libs v0.2.3
+require (
+	github.com/skowe/masterapp/libs v0.2.3
+	github.com/skowe/masterapp/models v0.1.1
+)
